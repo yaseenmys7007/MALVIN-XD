8eb3cc1345f8d097cfc1440bbabd386c31ed425c
Yaseen Siddiqui (MYS) <myaseensaddiquimys@gmail.com>
2025-07-25 12:26:54 +0500

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: "malvin~hjlXQKRJ#K8gBMkRJd-6t8GgNurHvS3Yl3k1R5f137RNmUHnQqS8",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "263714757857", // 923128609955
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
